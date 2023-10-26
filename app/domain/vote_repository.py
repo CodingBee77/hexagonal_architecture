b861b990bec52cbd05a96648725b7242b0e36ea5
martyna <codingbee77@tlen.pl>
2023-10-26 20:45:57 +0200

    Add vote repository interface

---
+++ app/domain/vote_repository.py
@@ -0,0 +1,17 @@
+import abc
+from typing import List
+from hexagonal_architecture.domain.vote import Vote
+
+
+class VoteRepository(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def add(self, vote: Vote) -> Vote:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def all(self) -> List[Vote]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def total(self) -> int:
+        raise NotImplementedError
